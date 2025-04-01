3d1e45b067f68a993235b36cd959b3d9185c73c6
ilisanbaev04 <138506617+ilisanbaev04@users.noreply.github.com>
2025-04-02 00:07:10 +0300

    changed: Sidebar and adDetails

--- RentFrontendAdmin/src/components/SearchBar.js
+++ RentFrontendAdmin/src/components/SearchBar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { FiSearch } from "react-icons/fi"; // Подключаем иконку
+import { FiSearch } from "react-icons/fi";
 import "../styles/searchBar.css";
 
 const SearchBar = ({ searchQuery, handleSearchChange }) => {
   return (
     <div className="search-bar">
-      <FiSearch className="search-icon" /> {/* Иконка лупы */}
+      <FiSearch className="search-icon" />
       <input
         type="text"
         placeholder="Поиск..."
