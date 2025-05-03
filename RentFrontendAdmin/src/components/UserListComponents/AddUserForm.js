7aede798bfb46f39172dff24dca2422ee6a43c36
ilisanbaev04 <138506617+ilisanbaev04@users.noreply.github.com>
2025-05-04 02:57:05 +0300

    (TP-75) feat: crud AddUserForm

--- RentFrontendAdmin/src/components/UserListComponents/AddUserForm.js
+++ RentFrontendAdmin/src/components/UserListComponents/AddUserForm.js
@@ -1,53 +1,109 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import Header from "../HeaderComponents/Header";
 import { ReactComponent as EditIcon } from '../../assets/EditIcon.svg';
-import { ReactComponent as Calendar } from '../../assets/Birthday.svg';
-import { ReactComponent as Arrow } from '../../assets/Chevrondown.svg';
+import userService from "../../api/userService";
+import {useNavigate} from "react-router-dom";
 
 
 const AddUserForm = ({ addNewUser, editingUser, onCancel, onDelete }) => {
-  const [formData, setFormData] = useState(
-    editingUser || {
-      email: "",
-      firstName: "",
-      lastName: "",
-      birthDate: "",
-      gender: "Не указан",
-      role: "Пользователь",
-      registrationDate: new Date().toISOString().split("T")[0],
-      photo: null,
-    }
-  );
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    birthDate: "",
+    gender: "",
+  });
+
+  useEffect(() => {
+    setFormData({
+      name: formData.name,
+      surname: formData.surname,
+      email: formData.email,
+      birthDate: formData.birthDate,
+      gender: formData.gender,
+    });
+  }, []);
+
+  const genderMapping = {
+    'Мужской': 'MALE',
+    'Женский': 'FEMALE',
+    'Не указан': ''
+  };
+
+  const reverseGenderMapping = {
+    'MALE': 'Мужской',
+    'FEMALE': 'Женский',
+    'UNSPECIFIED': 'Не указан'
+  };
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
-  
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+    setFormData(prev => ({ ...prev, [name]: value }));
   };
 
-  const handleImageChange = (e) => {
+  const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      setFormData((prevState) => ({
-        ...prevState,
-        photo: URL.createObjectURL(file),
-      }));
+      setSelectedFile(file);
+      setPreviewUrl(URL.createObjectURL(file));
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const newUser = {
-      id: editingUser ? editingUser.id : Date.now(),
-      ...formData,
-    };
-    addNewUser(newUser);
+    setLoading(true);
+    setError("");
+
+    try {
+
+      const gender = genderMapping[formData.gender] || "";
+      const formDataToSend = new FormData();
+
+      formDataToSend.append("name", formData.name);
+      formDataToSend.append("surname", formData.surname);
+      formDataToSend.append("email", formData.email);
+      formDataToSend.append("birthDate", formData.birthDate);
+
+      formDataToSend.append("gender", gender);
+
+      if (selectedFile) {
+        formDataToSend.append("file", selectedFile);
+      }
+
+      console.log(formData);
+      for (let [key, value] of formDataToSend.entries()) {
+        console.log(key, value);
+      }
+
+      const response = await userService.create(formDataToSend);
+
+      addNewUser({
+        ...response.data,
+        imageDTO: selectedFile
+            ? { url: URL.createObjectURL(selectedFile) }
+            : editingUser.imageDTO
+      });
+
+      onCancel();
+      navigate("/users", { state: { refresh: true } });
+
+    } catch (err) {
+      setError(err.response?.data?.message || "Ошибка обновления пользователя");
+      console.error("Ошибка обновления:", err);
+    } finally {
+      setLoading(false);
+    }
   };
+
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
@@ -55,7 +111,7 @@ const AddUserForm = ({ addNewUser, editingUser, onCancel, onDelete }) => {
   return (
     <div className="add-ad-container">
       <Header searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
-      <form className="add-user-form" onSubmit={handleSubmit}>
+      <form className="add-user-form">
         <h2>{ "Добавить пользователя"}</h2>
           
           <div className="profile-photo">
@@ -63,32 +119,30 @@ const AddUserForm = ({ addNewUser, editingUser, onCancel, onDelete }) => {
               <div className="photo-label">Фотография профиля</div>
               <div className="photo-container">
                 <div className="photo-preview">
-                  {formData.photo ? (
-                    <img src={formData.photo} alt="" />
+                  {previewUrl ? (
+                    <img src={previewUrl} alt="Photo" />
                   ) : (
                     <div className="empty-photo" />
                   )}
                 </div>
                 <label className="photo-edit-button">
-                  <input type="file" accept="image/*" onChange={handleImageChange} />
+                  <input type="file" accept="image/*" onChange={handleFileChange} />
                   <EditIcon width="20" height="20" />
                 </label>
               </div>
             </div>
-          </div>
-
-        <div className="fields-row">
+          </div><div className="fields-row">
           <div>
             <label className="column-name">Почта</label>
             <input type="email" name="email" value={formData.email} onChange={handleChange} required />
           </div>
           <div>
             <label className="column-name">Имя</label>
-            <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+            <input type="text" name="name" value={formData.name} onChange={handleChange} required />
           </div>
           <div>
             <label className="column-name">Фамилия</label>
-            <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
+            <input type="text" name="surname" value={formData.surname} onChange={handleChange} required />
           </div>
         </div>
 
@@ -106,15 +160,15 @@ const AddUserForm = ({ addNewUser, editingUser, onCancel, onDelete }) => {
           <div>
             <label className="column-name">Пол</label>
             <select name="gender" value={formData.gender} onChange={handleChange}>
+              <option value="Не указан">Не указан</option>
               <option value="Мужской">Мужской</option>
               <option value="Женский">Женский</option>
-              <option value="Не указан">Не указан</option>
             </select>
 
           </div>
           <div>
             <label className="column-name">Роль</label>
-            <select name="role" value={formData.role} onChange={handleChange}>
+            <select name="role" >
               <option value="Пользователь">Пользователь</option>
               <option value="Менеджер">Менеджер</option>
               <option value="Администратор">Администратор</option>
@@ -125,17 +179,17 @@ const AddUserForm = ({ addNewUser, editingUser, onCancel, onDelete }) => {
         <div className="fields-row">
           <div>
             <label className="column-name">Дата регистрации</label>
-            <input type="date" name="registrationDate" value={formData.registrationDate} onChange={handleChange} required />
+            <input type="date" name="registrationDate" required />
           </div>
-          <div></div> {/* Пустое место для равномерного распределения */}
-          <div></div> {/* Пустое место для равномерного распределения */}
+          <div></div>
+          <div></div>
         </div>
 
         <div className="footer-buttons">
           <button type="button" className="cancel-button" onClick={onCancel}>
             Отмена
           </button>
-          <button type="submit" className="submit-button">
+          <button type="submit" className="submit-button" onClick={handleSubmit}>
             Добавить
           </button>
         </div>
