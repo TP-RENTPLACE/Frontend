2ff4692a477df36f4bed911a5e4edc12fbeeb66e
ilisanbaev04 <138506617+ilisanbaev04@users.noreply.github.com>
2025-05-20 20:14:17 +0300

    (TP-76) feat: added methods for block and activate user

--- RentFrontendAdmin/src/api/userService.js
+++ RentFrontendAdmin/src/api/userService.js
@@ -18,11 +18,19 @@ class UserService {
     }
 
     async delete(userId) {
-        return await apiClient.delete(`/users/${userId}`, {authRequired: true});
+        return await apiClient.delete(`/users/admin/${userId}`, {authRequired: true});
     }
 
     async updateMe(userData) {
         return await apiClient.patch(`/users/me`, userData, {authRequired: true});
+    }
+
+    async blockUser(userId) {
+        return await apiClient.get(`/users/admin/block-user/${userId}`, {authRequired: true});
+    }
+
+    async activateUser(userId) {
+        return await apiClient.get(`/users/admin/activate-user/${userId}`, {authRequired: true});
     }
 }
 
